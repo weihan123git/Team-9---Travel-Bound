b3de8ec0f1c3f0898b5c91f2b86bc0885bbf2587
Wei-Han <chouweihan@gmail.com>
2018-12-03 17:58:11 -0800

    button changes

--- editEntryPage/editEntryPage.js
+++ editEntryPage/editEntryPage.js
@@ -6,9 +6,9 @@ var config = {
     storageBucket: "travelbound-c0cd0.appspot.com",
     messagingSenderId: "1038974227621"
     };
-  
+
     firebase.initializeApp(config);
-  
+
   var database = firebase.database();
 
 $(document).ready(function() {
@@ -26,9 +26,27 @@ function submitFunction() {
     document.getElementById("demo").innerHTML = txt;
 }
 
+$(function() {
+var i;
+var count = 0;
+var firebaseCount = 5;
+
+
+for (i = 0; i < firebaseCount; i++) {
+
+    var div1 = $("<div id='entryDiv' class='entryDivClass'></div>");
+    var someID = 'entryDiv' + count;
+    $(div1).attr('id', someID);
+    var div2 = $('<img id = "entryImageDiv" src = "https://dummyimage.com/80x80/4fa6a0/ffffff&text=Placeholder+Image">');
+    var div3 = $("<p id = 'entryTitleDiv'>Here is your last entry</p>");
+    $("#selectionBox").append(div1);
+    $("#entryDiv" + count).append(div2, div3);
+    count++;
+ }
+
 //create a new Entry
 function addNewEntry(){
-    
+
     var newEntry = {
         purchaseCurrency: "JPY",
         picture: "nothing for now",
@@ -44,20 +62,22 @@ function addNewEntry(){
     var i = 0;
 
     var entries = database.ref("Accounts").child("account1").child("journeys").child(("journey" + i)).child(("entry" + i));
-    
+
     entries.update(newEntry);
     console.log("Entry" + i + " successfully added to journey" + i);
-}   
+}
 
 // read from the database
 function readJourney(){
 
     var i = 0;
-  
+
     var entries = database.ref("Accounts").child("account1").child(("journey" + i)).child(("entry" + i));
-  
+
     entries.child("day2").once("value").then(function(x) {
       var journey = x.val();
       console.log(journey);
     })
   }
+
+})
